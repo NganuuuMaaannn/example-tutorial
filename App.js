3649ae5e63401378efe13aebf0fefe2570e5475c
NganuuuMaaannn <seanmichael.doinog@hcdc.edu.ph>
2025-08-30 00:46:47 +0800

    Added my name

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello World!</Text>
+      <Text>Ivanne</Text>
       <StatusBar style="auto" />
     </View>
   );
